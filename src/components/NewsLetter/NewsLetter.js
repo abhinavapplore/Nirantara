7bea377c7bb9acbc0c7ac53a77490f7b40539187
somethingofscientist <1shraddhanand@gmail.com>
2023-11-04 10:09:35 +0530

    c

--- src/components/NewsLetter/NewsLetter.js
+++ src/components/NewsLetter/NewsLetter.js
@@ -2,55 +2,62 @@ import React, { useState } from 'react'
 import styles from './NewsLetter.module.css';
 
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'; 
+import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
+import {AiOutlineLoading3Quarters} from 'react-icons/ai'
 
 const NewsLetter = () => {
   const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
   const backendUrl = 'https://nirantari-backend.vercel.app/subscriber'
   // const backendUrl = 'http://localhost:9000/subscriber'  
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-  try {
-    const response = await axios.post(backendUrl, { email });
-    console.log('Subscription successful:', response.data);
-    toast.success('Subscription successful', {
-      position: 'top-right',
-      autoClose: 3000, 
-    });
-    setEmail('');
-  } catch (error) {
-    console.error('Subscription failed:', error.message);
-    toast.error('Subscription failed. Please try again later.', {
-      position: 'top-right',
-      autoClose: 3000, 
-    });
-  }
-};
+    try {
+      setLoading(true); 
+      const response = await axios.post(backendUrl, { email });
+      console.log('Subscription successful:', response.data);
+      toast.success('Email sent successful', {
+        position: 'top-right',
+        autoClose: 3000,
+      });
+      setEmail('');
+    } catch (error) {
+      console.error('Subscription failed:', error.message);
+      toast.error('Subscription failed. Please check email.', {
+        position: 'top-right',
+        autoClose: 3000,
+      });
+    }finally {
+      setLoading(false); // Set loading to false when the request completes
+    }
+  };
 
   return (
     <div className={styles.container} id='newsletter'>
-    <div className={styles.left}>
-      sign up & stay informed.
+      <div className={styles.left}>
+        sign up & stay informed.
+      </div>
+      <div className={styles.right}>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder='Enter your Email Address'
+            value={email}
+            required
+            
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button className={styles.button} type="submit">
+            {/* Subscribe */}
+            {loading ? 'Loading...' : 'Subscribe'}
+          </button>
+        </form>
+      </div>
+      <ToastContainer />
     </div>
-    <div className={styles.right}>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder='Enter your Email Address'
-          value={email}
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button className={styles.button} type="submit">
-          Subscribe
-        </button>
-      </form>
-    </div>
-    <ToastContainer />
-  </div>
   )
 }
 
